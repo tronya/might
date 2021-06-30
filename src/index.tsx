421abc428410d5b0b7e39025102a227b8d77fd4e
y.troniak <y.troniak@levi9.com>
2021-06-30 17:57:34 +0300

    Trying to register SW

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {CssBaseline, MuiThemeProvider} from "@material-ui/core";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import {defaultTheme} from "./theme";
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
     <React.StrictMode>
@@ -17,7 +17,10 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-serviceWorker.register()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
